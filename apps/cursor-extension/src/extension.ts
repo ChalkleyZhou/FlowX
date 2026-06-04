b813fafda182eed425dbf447ea48fdda52afa1b1
zhoujinglei <241246590@qq.com>
2026-06-04 21:30:07 +0800

    feat(cursor): scaffold FlowX extension

---
+++ apps/cursor-extension/src/extension.ts
@@ -0,0 +1,31 @@
+import * as vscode from 'vscode';
+
+class FlowXPlaceholderProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
+  private readonly onDidChangeTreeDataEmitter = new vscode.EventEmitter<vscode.TreeItem | undefined>();
+
+  readonly onDidChangeTreeData = this.onDidChangeTreeDataEmitter.event;
+
+  refresh() {
+    this.onDidChangeTreeDataEmitter.fire(undefined);
+  }
+
+  getTreeItem(element: vscode.TreeItem) {
+    return element;
+  }
+
+  getChildren() {
+    const item = new vscode.TreeItem('Configure FlowX to load tasks', vscode.TreeItemCollapsibleState.None);
+    item.contextValue = 'flowxPlaceholder';
+    return [item];
+  }
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  const provider = new FlowXPlaceholderProvider();
+  context.subscriptions.push(
+    vscode.window.registerTreeDataProvider('flowxTasks', provider),
+    vscode.commands.registerCommand('flowx.refreshTasks', () => provider.refresh()),
+  );
+}
+
+export function deactivate() {}
